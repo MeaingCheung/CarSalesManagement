9a8b3ae23696e40da29dcb92555eea57332933b1
MeaingCheung <meaingcheung@gmail.com>
2017-02-27 11:33:53 +0800

    修改 pom文件，添加json依赖jar包，接口支持返回json数据

--- src/main/java/com/yyz/web/controller/BaseController.java
+++ src/main/java/com/yyz/web/controller/BaseController.java
@@ -1,16 +1,42 @@
 package com.yyz.web.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BaseController {
 
+	/**
+	 * 环境测试接口
+	 *
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping(value = "/test", method = { RequestMethod.GET, RequestMethod.POST })
 	public String test(HttpServletRequest request) {
 		return "test";
+	}
+
+	/**
+	 * 测试json数据的返回
+	 *
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/testJson", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public Object testJson(HttpServletRequest request) {
+		Map<String, Object> obj = new HashMap<String, Object>();
+		obj.put("k1", "v1");
+		obj.put("k2", "v2");
+		obj.put("k3", "v3");
+		return obj;
 	}
 }
